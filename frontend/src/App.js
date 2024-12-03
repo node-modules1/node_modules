2e6069d46dd35c83f5d45910d26765553daf9e72
node-modules1 <vedthorat027@gmail.com>
2024-12-03 22:43:22 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import {Routes,Route} from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NewRecipe from './components/NewRecipe';
+import './components/styles.css'
+import EditRecipe from './components/EditRecipe';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/recipe/add" element={<NewRecipe></NewRecipe>}></Route>
+      <Route path='edit/:id' element={<EditRecipe></EditRecipe>}></Route>
+    </Routes>
+    </div>
+
+  );
+}
+
+export default App;
